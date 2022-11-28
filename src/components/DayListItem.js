7d750044479b578d411d0893f4c1afd856343245
Stefan Talbot <satalbot@protonmail.com>
2022-11-28 18:41:36 -0500

    Connect Props
    + Added: Prop values to DayListItem.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function DayListItem(props) {
+  const { name, spots, selected, setDay} = props;
   return (
-    <li>
-      <h2 className='text--regular'>Day Name</h2>
-      <h3 className='text--light'>X spots remaining</h3>
+    <li onClick={() => setDay(name)}>
+      <h2 className='text--regular'>{name}</h2>
+      <h3 className='text--light'>{spots} spots remaining</h3>
     </li>
   );
 }
