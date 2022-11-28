7ab4eb7e109ab45d939153c82b50723adbf72a64
Stefan Talbot <satalbot@protonmail.com>
2022-11-28 18:59:18 -0500

    Add Styling
    + Added: Styling to DayListItem.
    + Added: Conditional styling to DayListItem.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,18 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
   const { name, spots, selected, setDay} = props;
+  const isFull = spots < 1;
+  const dayListItemClass = classNames('day-list__item', 
+  {
+    'day-list__item--selected': selected,
+    'day-list__item--full': isFull
+  });
+
   return (
-    <li onClick={() => setDay(name)}>
+    <li className={dayListItemClass} onClick={() => setDay(name)}>
       <h2 className='text--regular'>{name}</h2>
       <h3 className='text--light'>{spots} spots remaining</h3>
     </li>
