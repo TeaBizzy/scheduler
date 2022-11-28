49c96e0520a3bc391edb6ca0718d5c1e15a28d12
Stefan Talbot <satalbot@protonmail.com>
2022-11-28 18:05:58 -0500

    Add Button Components
    + Added: Button component.
    + Added: Conditional styling to button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from "classnames";
+import { action } from "@storybook/addon-actions";
+import { act } from "@testing-library/react";
 
 export default function Button(props) {
-   return <></>;
+   const { confirm, danger, disabled, onClick } = props
+   let buttonClass = classNames('button', {'button--confirm': confirm, 'button--danger': danger});
+
+   return (
+      <button 
+         className={buttonClass}
+         onClick={onClick}
+         disabled={disabled}
+         >
+            {props.children}
+         </button>
+   );
 }
