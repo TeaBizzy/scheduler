a81a312c31aae9e11e7825c85c68d56312f127ad
Stefan Talbot <satalbot@protonmail.com>
2022-11-28 20:05:57 -0500

    Add State
    + Added: Day state to application.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
@@ -23,6 +23,9 @@ const days = [
 ];
 
 export default function Application(props) {
+
+  const [day, setDay] = useState('Monday');
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -36,8 +39,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList 
             days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
+            day={day}
+            setDay={day => setDay(day)}
           />
         </nav>
         <img
