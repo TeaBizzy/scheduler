b6499dfdf1de63fb22d04948e1b24e180aa134bc
Stefan Talbot <satalbot@protonmail.com>
2022-11-29 10:29:08 -0500

    Add Conditional Styling, And Data
    + Added: Conditional styling for selected.
    ~ Modified: Interviewer name only appears if selected.

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,18 +1,19 @@
 import React from "react";
-import "components/InterviewerListItem.scss"
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
 
 export default function InterviewerListItem (props) {
-
-  const { id, name, avatar } = props;
+  const { id, name, avatar, selected, setInterviewer } = props;
+  const interviewerListItemClasses = classNames("interviewers__item", {"interviewers__item--selected": selected})
 
   return (
-    <li key={id} className="interviewers__item" onClick={() => {setInterviewer(id)}}>
+    <li key={id} className={interviewerListItemClasses} onClick={() => {setInterviewer(id)}}>
       <img
-        className="interviews__item-image"
+        className="interviewers__item-image"
         src={avatar}
         alt={name}
       />
-      {name}
+      {selected && name}
     </li>
   );
 }
