c3c205ec0821627e5a72da8e0b21d6087bc93bfd
Stefan Talbot <satalbot@protonmail.com>
2022-11-28 19:13:10 -0500

    Bug Fix
    * Fixed: Spots remaining text to display correct string based on remaining spots.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -11,10 +11,21 @@ export default function DayListItem(props) {
     'day-list__item--full': isFull
   });
 
+  const formatSpots = function() {
+    switch (spots) {
+      case 1:
+        return `1 spot remaining`;
+      case 0:
+        return 'no spots remaining';
+      default:
+        return `${spots} spots remaining`;
+    }
+  };
+
   return (
     <li className={dayListItemClass} onClick={() => setDay(name)}>
       <h2 className='text--regular'>{name}</h2>
-      <h3 className='text--light'>{spots} spots remaining</h3>
+      <h3 className='text--light'>{formatSpots()}</h3>
     </li>
   );
 }
