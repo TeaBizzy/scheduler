d0273be419127d9f621c8fc468ee8e2dfb439e88
Stefan Talbot <satalbot@protonmail.com>
2022-11-29 18:15:28 -0500

    Populate Component
    + Added: Populated JSX with prop values.

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 export default function(props) {
+
+  const { student, interviewer, onEdit, onDelete} = props;
   
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
-        <h2 className="text--regular">Lydia Miller-Jones</h2>
+        <h2 className="text--regular">{student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">Sylvia Palmer</h3>
+          <h3 className="text--regular">{interviewer.name}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
@@ -17,11 +19,13 @@ export default function(props) {
             className="appointment__actions-button"
             src="images/edit.png"
             alt="Edit"
+            onClick={onEdit}
           />
           <img
             className="appointment__actions-button"
             src="images/trash.png"
             alt="Delete"
+            onClick={onDelete}
           />
         </section>
       </section>
