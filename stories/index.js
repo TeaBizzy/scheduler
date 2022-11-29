e6a2a3dc75cbabf06780ed8aaaaa8dc0f3284b1a
Stefan Talbot <satalbot@protonmail.com>
2022-11-29 18:37:18 -0500

    Add Story
    + Added: Story for Error component.

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -178,5 +179,11 @@ storiesOf("InterviewerList", module)
     .add("Status", () => 
       <Status 
         message={'Deleting'}
+      />
+    )
+    .add("Error", () => 
+      <Error 
+        message={'Could not delete appointment.'}
+        onClose={action('onClose')}
       />
     )
