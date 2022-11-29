a80dfdbb2182d5d65850ef36b12d29583eb21ca7
Stefan Talbot <satalbot@protonmail.com>
2022-11-28 19:35:58 -0500

    Add DayListItem To DayList
    + Added: DayListItem to DayList.
    + Added: Each DayListItem is populated and rendered.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,8 +1,23 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList() {
+export default function DayList(props) {
+
+  const {days, day, setDay} = props;
+
+  const dayComponents = days.map((element) => 
+    <DayListItem
+      key={element.id}
+      name={element.name}
+      spots={element.spots}
+      selected={element.name === day}
+      setDay={setDay}
+    />
+  );
+
   return (
-    <ul></ul>
+    <ul>
+      {dayComponents}
+    </ul>
   );
 }
