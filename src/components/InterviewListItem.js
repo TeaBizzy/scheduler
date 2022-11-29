6a62849b468224d15e468b8f6b7898bbd4ad1c29
Stefan Talbot <satalbot@protonmail.com>
2022-11-29 09:44:13 -0500

    Add Component
    + Added: InterviewerListItem component skeleton.

---
+++ src/components/InterviewListItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function InterviewerListItem (props) {
+
+  const { id, name, avatar } = props;
+
+  return (
+    <li className="interviewers__item">
+      <img
+        className="interviews__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      Sylvia Palmer
+    </li>
+  );
+}
