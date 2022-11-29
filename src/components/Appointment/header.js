471e6fe9ff903f1ac90e90af651da65e82648a81
Stefan Talbot <satalbot@protonmail.com>
2022-11-29 17:34:20 -0500

    Populate Header Component
    + Added: Time prop to Header component.

--- src/components/Appointment/header.js
+++ src/components/Appointment/header.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 export default function Header(props) {
-  
+
+  const { time } = props;
+
   return (
     <header className="appointment__time">
-      <h4 className="text--semi-bold">12pm</h4>
+      <h4 className="text--semi-bold">{time}</h4>
       <hr className="appointment__separator" />
     </header>
   );
