61d667a19de6d27e8ac43daac3006c8321ac4b58
Stefan Talbot <satalbot@protonmail.com>
2022-11-29 18:25:35 -0500

    Populate Component
    + Added: Populated component with prop values.

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -3,12 +3,14 @@ import Button from "components/Button";
 
 export default function Confirm(props) {
   
+  const { message, onConfirm, onCancel } = props;
+
   return (
     <main className="appointment__card appointment__card--confirm">
-      <h1 className="text--semi-bold">Delete the appointment?</h1>
+      <h1 className="text--semi-bold">{message}</h1>
       <section className="appointment__actions">
-        <Button danger>Cancel</Button>
-        <Button danger>Confirm</Button>
+        <Button danger onClick={onCancel}>Cancel</Button>
+        <Button danger onClick={onConfirm}>Confirm</Button>
       </section>
     </main>
   );
