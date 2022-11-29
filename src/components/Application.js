bad31d0acd0c635fae081fa2fb262ea9f383065c
Stefan Talbot <satalbot@protonmail.com>
2022-11-28 19:41:36 -0500

    Add DayList To Application
    + Added: Mock data for DayList component.
    + Added: DayList component to Application.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,26 @@
 import React from "react";
 
 import "components/Application.scss";
+import DayList from "./DayList";
+
+// MOCK DATA TODO: Remove this!!
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
   return (
@@ -13,7 +33,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList 
+            days={days}
+            day={"Monday"}
+            setDay={day => console.log(day)}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
