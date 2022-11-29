135c06b853c1d5c1388495aed2b1c8875d1561c7
Stefan Talbot <satalbot@protonmail.com>
2022-11-29 18:14:49 -0500

    Add Stories
    + Added: Story for Show component.

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -155,5 +156,13 @@ storiesOf("InterviewerList", module)
     .add("Empty", () =>
       <Empty 
         onAdd={action('onAdd')}
+      />
+    )
+    .add("Show", () => 
+      <Show 
+        student={'Lydia Miller-Jones'}
+        interviewer={interviewer}
+        onEdit={action('onEdit')}
+        onDelete={action('onDelete')}
       />
     )
