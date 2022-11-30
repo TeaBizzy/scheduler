66752b63c7db67e2115812f23e0c3a5085f0c22a
Stefan Talbot <satalbot@protonmail.com>
2022-11-29 19:30:20 -0500

    Add Stories
    + Added: Stories for the Form component.

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form"
 
 storiesOf("Button", module)
   .addParameters({
@@ -140,50 +141,66 @@ storiesOf("InterviewerList", module)
     />
   ));
 
-  storiesOf("Appointment", module)
-    .addParameters({
-      backgrounds: [{name: 'white', value: '#fff', default: true}]
-    })
-    .add("Appointment", () => 
-      <Appointment />
-    )
-    .add("Appointment with Time", () => 
-      <Appointment 
-        time={'12pm'}
-    />)
-    .add("Header", () => 
-      <Header
-        time={'12pm'}
-      />
-    )
-    .add("Empty", () =>
-      <Empty 
-        onAdd={action('onAdd')}
-      />
-    )
-    .add("Show", () => 
-      <Show 
-        student={'Lydia Miller-Jones'}
-        interviewer={interviewer}
-        onEdit={action('onEdit')}
-        onDelete={action('onDelete')}
-      />
-    )
-    .add("Confirm", () =>
-      <Confirm
-        message={'Delete the appointment?'}
-        onConfirm={action('onConfirm')}
-        onCancel={action('onCancel')}
-      />
-    )
-    .add("Status", () => 
-      <Status 
-        message={'Deleting'}
-      />
-    )
-    .add("Error", () => 
-      <Error 
-        message={'Could not delete appointment.'}
-        onClose={action('onClose')}
-      />
-    )
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{name: 'white', value: '#fff', default: true}]
+  })
+  .add("Appointment", () => 
+    <Appointment />
+  )
+  .add("Appointment with Time", () => 
+    <Appointment 
+      time={'12pm'}
+  />)
+  .add("Header", () => 
+    <Header
+      time={'12pm'}
+    />
+  )
+  .add("Empty", () =>
+    <Empty 
+      onAdd={action('onAdd')}
+    />
+  )
+  .add("Show", () => 
+    <Show 
+      student={'Lydia Miller-Jones'}
+      interviewer={interviewer}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  )
+  .add("Confirm", () =>
+    <Confirm
+      message={'Delete the appointment?'}
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
+    />
+  )
+  .add("Status", () => 
+    <Status 
+      message={'Deleting'}
+    />
+  )
+  .add("Error", () => 
+    <Error 
+      message={'Could not delete appointment.'}
+      onClose={action('onClose')}
+    />
+  )
+  .add("Edit", () =>
+    <Form 
+      student={'Stefan Talbot'}
+      interviewer={3}
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  )
+  .add("Create", () => 
+    <Form 
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  )
