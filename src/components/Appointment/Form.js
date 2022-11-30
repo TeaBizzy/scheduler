3cf39b7a61378dd5ccdfa8a52637a9c5db42bc53
Stefan Talbot <satalbot@protonmail.com>
2022-11-29 20:01:19 -0500

    Add Functions To Component
    + Added: Cancel & reset functions to the Form component.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -9,6 +9,16 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = function() {
+    setStudent('');
+    setInterviewer(null);
+  };
+
+  const cancel = function() {
+    reset();
+    onCancel();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -30,7 +40,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>Cancel</Button>
+          <Button danger onClick={() => cancel()}>Cancel</Button>
           <Button confirm onClick={onSave}>Save</Button>
         </section>
       </section>
