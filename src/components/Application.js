67c5b96feb55340a859d3161682054fc838440db
Stefan Talbot <satalbot@protonmail.com>
2022-11-30 16:49:47 -0500

    Refactored States
    # Refactored: day and days state into one state.

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,8 +47,15 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days:[],
+    appointments: {}
+  });
+
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({...prev, days}));
+
 
   useEffect(() => {
     axios.get(`/api/days`)
@@ -67,8 +74,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList 
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={day => setDay(day)}
           />
         </nav>
