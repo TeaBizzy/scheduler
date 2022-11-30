98309c38087a45ac9ae2c28d83603d2ae83d76e4
Stefan Talbot <satalbot@protonmail.com>
2022-11-29 21:53:49 -0500

    Add Story
    + Added: Story for Empty Appointment.

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -204,3 +204,9 @@ storiesOf("Appointment", module)
       onCancel={action('onCancel')}
     />
   )
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
