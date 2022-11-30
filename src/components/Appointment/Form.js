499f1e7fae3be2df7e63d45a12f5631c945956c0
Stefan Talbot <satalbot@protonmail.com>
2022-11-29 19:18:20 -0500

    Add Component
    + Added: Form component skeleton.

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,34 @@
+import React from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function Form(props) {
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+              This must be a controlled component
+              your code goes here
+            */
+          />
+        </form>
+        <InterviewerList 
+          /* your code goes here */
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger /* your code goes here */>Cancel</Button>
+          <Button confirm /* your code goes here */>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
