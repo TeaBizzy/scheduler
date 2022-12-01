86e75c308b166f3d0977b2bd2fb7a8649efb3200
Stefan Talbot <satalbot@protonmail.com>
2022-11-30 19:27:15 -0500

    Add State And API Request
    + Added: Interviewers to state.
    + Added: API request to GET /api/interviewers.

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,26 +4,40 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios, { Axios } from "axios";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
 
   const [state, setState] = useState({
     day: "Monday",
     days:[],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
-  let dailyAppointments = getAppointmentsForDay(state, state.day);
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment 
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   const setDay = (day) => {
     setState(prev => ({...prev, day}))
   };
 
   useEffect(() => {
-    Promise.all([axios.get(`/api/days`), axios.get(`/api/appointments`)])
+    Promise.all([axios.get(`/api/days`), axios.get(`/api/appointments`), axios.get(`/api/interviewers`)])
     .then((values) => {
-      setState(prev => ({...prev, days: values[0].data, appointments: values[1].data}))
+      setState(prev => ({...prev, days: values[0].data, appointments: values[1].data, interviewers: values[2].data}))
     })
   }, []);
 
@@ -51,7 +65,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map((appointment) => {
+        {appointments.map((appointment) => {
           return (
 
             <Appointment 
