a5ee17c04ad9ec39f3827f6aae56dfdb9f3d11d8
Stefan Talbot <satalbot@protonmail.com>
2022-12-01 19:25:13 -0500

    Add Function
    + Added: transition function to the returned object.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,11 @@
 import { useState } from "react"
 
-export default function useVisualMode(initial) {
+export function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  return { mode };
+  function transition(mode) {
+    setMode(mode);
+  }
+
+  return { mode, transition };
 }
