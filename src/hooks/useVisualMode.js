0f00e499e5faa728f37af009fe9e9e5b53e2edb6
Stefan Talbot <satalbot@protonmail.com>
2022-12-01 19:58:09 -0500

    Implement Function
    + Added: back(), returns the mode to the previous mode.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,21 @@ import { useState } from "react"
 
 export function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   function transition(mode) {
     setMode(mode);
+    const newHistory = [...history, mode]
+    setHistory(newHistory);
   }
 
-  return { mode, transition };
+  function back() {
+    if(history.length < 2) return;
+
+    const previousMode = history[history.length - 2];
+    setHistory(history.slice(0, history.length-1));
+    setMode(previousMode);
+  }
+
+  return { mode, transition, back };
 }
