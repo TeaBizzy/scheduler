2a4dee00f7e4e87d96113d7f91c9af5c70d2a008
Stefan Talbot <satalbot@protonmail.com>
2022-12-06 22:38:41 -0500

    Add Test
    + Added: Test for submitting the name entered by user.

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -62,4 +62,18 @@ describe("Form", () => {
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
+
+
+  it("submits the name entered by the user", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText } = render (<Form interviewers={interviewers} onSave={onSave} interviewer={1}/>)
+
+    const input = getByPlaceholderText("Enter Student Name");
+
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones"} });
+    fireEvent.click(getByText("Save"));
+
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
+  });
 });
