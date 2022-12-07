860fbc1bd71e7383197ebd2c1c6753944cb0587d
Stefan Talbot <satalbot@protonmail.com>
2022-12-07 01:10:06 -0500

    Refactor
    # Refactored: Test to use async await.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,12 +7,13 @@ import Application from "components/Application";
 afterEach(cleanup);
 
 describe("Application", () => {
-  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
     
-    return waitForElement(() => getByText("Monday")).then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    });
+    await waitForElement(() => getByText("Monday"));
+
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 })
