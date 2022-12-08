e1f5faafd5b2bdd8dfce7ac8defa50f770555ebb
Stefan Talbot <satalbot@protonmail.com>
2022-12-07 21:31:00 -0500

    Refactor
    # Refactored: Test to use best practices.

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -7,6 +7,8 @@ describe("Navigation", () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/");
 
-    cy.contains("Tuesday").click();
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
   });
 });
