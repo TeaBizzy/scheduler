e6d4b254f2754d261c820c63e9b2ae832abd13bb
Stefan Talbot <satalbot@protonmail.com>
2022-12-07 21:52:47 -0500

    Add Test
    + Added: test should book an interview.

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,24 @@
+beforeEach(() => {
+  cy.request("/api/debug/reset");
+  cy.visit("/");
+  cy.contains("Monday");
+});
+
+describe("Appointments", () => {
+  it("should book an interview", () => {
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+    
+      cy.get("[data-testid=student-name-input]")
+        .type("Lydia Miller-Jones");
+
+      cy.get("[alt='Sylvia Palmer']")
+        .click();
+
+      cy.contains("Save")
+        .click();
+      
+      cy.contains(".appointment__card--show", "Lydia Miller-Jones", "Sylvia Palmer")
+  });
+});
